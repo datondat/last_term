48fe9ffaee23e8fc2641f598156477801035108e
béo <dat162561@gmail>
2025-05-14 09:35:16 +0700

    first commit

--- project/frame2.py
+++ project/frame2.py
@@ -6,6 +6,17 @@ class working:
     def create(m):
         m.destroy()
         m= tk.Tk()
+        width1=m.winfo_screenwidth()
+        height1=m.winfo_screenheight()
+        m.geometry("%dx%d"%(width1,height1))
+        for row in range(5):
+            for col in range(5):
+                tk.Button(
+                    master=m,
+                    text=f"Room ({(row+1)*(col+1)})",
+                    width=41,
+                    height=10,
+                ).grid(row=row,column=col,padx=3,pady=2)
         m.focus()
         m.mainloop()
     #-------------
